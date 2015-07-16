8b6049bb38f946d011f14abdab0a2084974ccc70
roophanxp <roophanxp@gmail.com>
2015-07-16 15:28:26 -0700

    Initial commit

---
+++ android/src/com/jnv/Bertrayal/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jnv.Bertrayal.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jnv.Bertrayal.Betrayal;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Betrayal(), config);
+	}
+}
---
+++ desktop/src/com/jnv/Bertrayal/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jnv.Bertrayal.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jnv.Bertrayal.Betrayal;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Betrayal(), config);
+	}
+}
