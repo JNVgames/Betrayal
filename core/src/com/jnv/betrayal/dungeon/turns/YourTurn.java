3978c26b43165b8c17c190b5eed24bb6902a9696
Your Name <vwbluetv@yahoo.com>
2016-08-07 21:35:54 -0700

    fixed your turn control panel graying out at bad times

--- core/src/com/jnv/betrayal/dungeon/turns/YourTurn.java
+++ core/src/com/jnv/betrayal/dungeon/turns/YourTurn.java
@@ -25,8 +25,51 @@ import java.util.List;
 
 public class YourTurn extends Turn {
 
+	private boolean isFirstAppearance = true;
+
 	public YourTurn(Field field, Pool<Label> panelPool, Pool<Button> buttonPool, Group panels, Betrayal game) {
 		super(field, panelPool, buttonPool, panels, game);
+	}
+
+	private void drawMainBar() {
+		panels.clearChildren();
+		panels.addActor(createPanel("Items", FontManager.getFont70(), Panel.bottomLeft, new Runnable() {
+			public void run() {
+				new DungeonInventory(gsm.game, field.getCurrentCard());
+				// todo change input to what is needed by the specific item
+			}
+		}));
+		panels.addActor(createPanel("Attack", FontManager.getFont70(), Panel.topLeft, new Runnable() {
+			public void run() {
+				field.beginSelectMode(1);
+				drawSelectBar(EventType.ATTACK);
+			}
+		}));
+		panels.addActor(createPanel("Defend", FontManager.getFont70(), Panel.topRight, new Runnable() {
+			public void run() {
+				field.beginSelectMode(1);
+				drawSelectBar(EventType.DEFEND);
+			}
+		}));
+		panels.addActor(createPanel("Flee", FontManager.getFont70(), Panel.bottomRight, new Runnable() {
+			@Override
+			public void run() {
+				new Confirmation(gsm.game, "Flee? 25% Chance") {
+					@Override
+					public void doAction() {
+						panels.clearChildren();
+						panels.addActor(createGrayPanel("Attempting to flee...", FontManager.getDarkFont70(), Panel.full));
+						if (PlayerCard.canFlee(1)) {
+							Effect effect = new Flee(field.getCurrentCard());
+							field.roundManager.addEvent(effect, effect.getStartType());
+						} else {
+							Effect effect = new FailedToFlee(field.getCurrentCard());
+							field.roundManager.addEvent(effect, effect.getStartType());
+						}
+					}
+				};
+			}
+		}));
 	}
 
 	private void drawSelectBar(final EventType eventType) {
@@ -56,50 +99,20 @@ public class YourTurn extends Turn {
 	@Override
 	public void draw() {
 		panels.clearChildren();
-		panels.addActor(createGrayPanel("Items", FontManager.getDarkFont70(), Panel.bottomLeft));
-		panels.addActor(createGrayPanel("Attack", FontManager.getDarkFont70(), Panel.topLeft));
-		panels.addActor(createGrayPanel("Defend", FontManager.getDarkFont70(), Panel.topRight));
-		panels.addActor(createGrayPanel("Flee", FontManager.getDarkFont70(), Panel.bottomRight));
-		panels.addAction(Actions.delay(1.5f, Actions.run(new Runnable() {
-			@Override
-			public void run() {
-				panels.clearChildren();
-				panels.addActor(createPanel("Items", FontManager.getFont70(), Panel.bottomLeft, new Runnable() {
-					public void run() {
-						new DungeonInventory(gsm.game, field.getCurrentCard());
-						// todo change input to what is needed by the specific item
-					}
-				}));
-				panels.addActor(createPanel("Attack", FontManager.getFont70(), Panel.topLeft, new Runnable() {
-					public void run() {
-						field.beginSelectMode(1);
-						drawSelectBar(EventType.ATTACK);
-					}
-				}));
-				panels.addActor(createPanel("Defend", FontManager.getFont70(), Panel.topRight, new Runnable() {
-					public void run() {
-						field.beginSelectMode(1);
-						drawSelectBar(EventType.DEFEND);
-					}
-				}));
-				panels.addActor(createPanel("Flee", FontManager.getFont70(), Panel.bottomRight, new Runnable() {
-					@Override
-					public void run() {
-						new Confirmation(gsm.game, "Flee? 25% Chance") {
-							@Override
-							public void doAction() {
-								if (PlayerCard.canFlee(1)) {
-									Effect effect = new Flee(field.getCurrentCard());
-									field.roundManager.addEvent(effect, effect.getStartType());
-								} else {
-									Effect effect = new FailedToFlee(field.getCurrentCard());
-									field.roundManager.addEvent(effect, effect.getStartType());
-								}
-							}
-						};
-					}
-				}));
-			}
-		})));
+		if (isFirstAppearance) {
+			panels.addActor(createGrayPanel("Items", FontManager.getDarkFont70(), Panel.bottomLeft));
+			panels.addActor(createGrayPanel("Attack", FontManager.getDarkFont70(), Panel.topLeft));
+			panels.addActor(createGrayPanel("Defend", FontManager.getDarkFont70(), Panel.topRight));
+			panels.addActor(createGrayPanel("Flee", FontManager.getDarkFont70(), Panel.bottomRight));
+			panels.addAction(Actions.delay(1.5f, Actions.run(new Runnable() {
+				@Override
+				public void run() {
+					drawMainBar();
+				}
+			})));
+			isFirstAppearance = false;
+		} else {
+			drawMainBar();
+		}
 	}
 }
